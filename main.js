9d4fb3f63660e9979b86f135e3fb843c295f97c2
Bart Westenenk <bart.westenenk@gmail.com>
2019-09-04 17:23:06 +0200

    Reset main.js

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-/*boolean gpConnected;
-var gamepad;
-
-window.addEventListener("gamepadconnected", function(e) {
-  console.log("Gamepad connected from index %d: %s",
-    e.gamepad.index, e.gamepad.id);
-  gpConnected = true;
-  gamepad =	e.gamepad;
-});
-
-window.addEventListener("gamepaddisconnected", function(e) {
-  console.log("Gamepad disconnected from index %d: %s",
-    e.gamepad.index, e.gamepad.id);
-  gpConnected = false;
-  gamepad = null;
-});
-*/
 (function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
@@ -27,22 +10,14 @@ window.addEventListener("gamepaddisconnected", function(e) {
 
     ext.my_first_block = function() {
         // Code that gets executed when the block is run
-		return 100;
     };
-	
-	ext.check_connection = function() {
-		//Code to check if there is an active connnection with a gamepad.
-		return gpConnected;		
-	}
-	
-	
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
-            ['r', 'Left stick X', 'my_first_block'],
-			['r', 'Gamepad connected?', 'check_connection'],
+            [' ', 'my first block', 'my_first_block'],
+        ]
     };
 
     // Register the extension
